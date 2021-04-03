0eaa03ed4adc84cabd338c1072284dbeeefb6578
ghaziNahdi94 <ghazinehdi@gmail.com>
2021-04-03 23:26:24 +0200

    hide suggestions friends

--- script.js
+++ script.js
@@ -6,7 +6,6 @@
  const hideHTMLElement = (element) => {
     if(element){
         const toHide = element[0] ? element[0] : element;
-        console.log(toHide);
         toHide.style && putCssAttributeValue(toHide, "display", "none");
     } 
 };
@@ -15,14 +14,43 @@ const putCssAttributeValue = (htmlElement, attribute, value) => {
     htmlElement.style[attribute] = value;
 };
 
+function getElementByXpath(path) {
+    return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+}
 /****************************************************************************************
  * Main
  ****************************************************************************************/
 var storiesClassName = "d2edcug0 e3xpq0al v8c10jal ejjq64ki";
 var stories =  document.getElementsByClassName(storiesClassName);
 
-var friendsSuggestionsClassName = "q5bimw55 gvuykj2m rq0escxv j83agx80 nvd5e08m dlv3wnog rl04r1d5 d76ob5m9 eg9m0zos tu1s4ah4 cxgpxx05 rx6hrq12 fq9i8bis cvgtb6nh ofs802cu g0qnabr5 g2wf7z4h pohlnb88 dkue75c7 mb9wzai9 tu18w1b4";
+var friendsSuggestionsClassName = "dati1w0a e5nlhep0 hv4rvrfc pybr56ya i1fnvgqd btwxx1t3 j83agx80";
 var friendsSuggestions =  document.getElementsByClassName(friendsSuggestionsClassName);
+console.log(friendsSuggestions);
+for(let i=0; i<friendsSuggestions.length; i++){
+    console.log(i + friendsSuggestions[i]);
+    hideHTMLElement(friendsSuggestions[i]);
+}
 
 hideHTMLElement(stories);
-hideHTMLElement(friendsSuggestions);
+
+const getSuggestionsDiv = () => {
+    const aTags = document.getElementsByTagName("span");
+    const searchText = "Connaissez-vous...";
+    let div = null;
+    
+    for (var i = 0; i < aTags.length; i++) {
+      if (aTags[i].textContent == searchText) {
+        div = aTags[i];
+        break;
+      }
+    }
+
+    return div?.parentElement?.parentElement?.parentElement?.parentElement;
+}
+setInterval(() => {
+    const suggestionsDiv = getSuggestionsDiv();
+    if(suggestionsDiv) {
+        console.log(suggestionsDiv);
+        hideHTMLElement(suggestionsDiv);
+    }
+}, 10000);
